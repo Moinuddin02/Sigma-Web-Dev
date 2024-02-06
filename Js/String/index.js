4be5b70f6d5c6358e94710eee4894f8ccb4869d1
Moinuddin02 <145395473+Moinuddin02@users.noreply.github.com>
2024-02-06 20:41:43 +0530

    Add files via upload

---
+++ Js/String/index.js
@@ -0,0 +1,28 @@
+let a="Moinuddin"; // String Name
+console.log(a[0]);   // It access the char by using index Number 
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length);// It show the Length of the string by using Length method.
+
+let b="Moinuddin";
+let c= "22";
+console.log(`My name is ${b} and age ${c}`);  // String Interpolation.
+
+let d="Moinuddin";
+console.log(d.toUpperCase());
+console.log(d.toLowerCase());
+console.log(d.slice(0,4));
+console.log(d.replace("Moinuddin", "Moin"));
+console.log(d.charAt(0));
+console.log(d.startsWith("Mo"));
+console.log(d.endsWith("in"));
+console.log(d.indexOf("M"));
+
+let firstname="Moinuddin";
+let lastname="Qureshi";
+
+console.log(firstname.concat(lastname)); // It concat the two or more strings....But It does not change the orignal string. It will be Remain Same.
+
